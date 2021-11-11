2f4ad47730e060595561f94956ddac18cd6e6580
Stambulo <stambulo@yandex.ru>
2021-11-11 16:48:36 +0200

    save all

--- app/src/main/java/com/stambulo/themovie/model/repository/MovieRepository.kt
+++ app/src/main/java/com/stambulo/themovie/model/repository/MovieRepository.kt
@@ -4,7 +4,6 @@ import com.stambulo.themovie.model.api.ApiService
 import com.stambulo.themovie.model.entity.Discover
 import com.stambulo.themovie.model.entity.Video
 import io.reactivex.rxjava3.core.Single
-import retrofit2.Response
 
 class MovieRepository(private val api: ApiService) {
     fun getMovies(page: Int): Single<Discover>{
