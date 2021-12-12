8de3276f80e21457ae8e344c6f70b81ca920770b
Stambulo <stambulo@yandex.ru>
2021-12-12 18:40:31 +0200

    Details Screen is done

--- app/src/main/java/com/stambulo/themovie/view/fragment/MovieListFragment.kt
+++ app/src/main/java/com/stambulo/themovie/view/fragment/MovieListFragment.kt
@@ -9,29 +9,39 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
+import com.stambulo.themovie.MovieApplication
 import com.stambulo.themovie.databinding.FragmentMovieListBinding
 import com.stambulo.themovie.model.api.ApiHolder
 import com.stambulo.themovie.model.api.ApiService
 import com.stambulo.themovie.model.api.RepositoryImpl
+import com.stambulo.themovie.model.entity.VideoItem
 import com.stambulo.themovie.view.MovieLoadStateAdapter
 import com.stambulo.themovie.view.MoviesAdapter
+import com.stambulo.themovie.view.OnListItemClickListener
+import com.stambulo.themovie.view.navigation.screens.Screens
 import com.stambulo.themovie.viewmodel.VideosViewModel
 import com.stambulo.themovie.viewmodel.VideosViewModelFactory
 import kotlinx.coroutines.flow.collectLatest
+import ru.terrakok.cicerone.Router
+import javax.inject.Inject
 
-class MovieListFragment: Fragment() {
+class MovieListFragment: Fragment(){
 
+    @Inject lateinit var router: Router
     private val api: ApiService = ApiHolder.apiService
     private val repository = RepositoryImpl(api)
     private lateinit var viewModel: VideosViewModel
     private var _viewBinding: FragmentMovieListBinding? = null
     private val viewBinding get() = checkNotNull(_viewBinding)
-    private var adapter = MoviesAdapter()
+    private val adapter by lazy(LazyThreadSafetyMode.NONE){
+        MoviesAdapter(onListItemClickListener)
+    }
 
     companion object { fun newInstance() = MovieListFragment() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        MovieApplication.instance.appComponent.inject(this)
         _viewBinding = FragmentMovieListBinding.inflate(inflater, container, false)
         return _viewBinding?.root
     }
@@ -39,7 +49,7 @@ class MovieListFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val factory = VideosViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, factory).get(VideosViewModel::class.java)
+        viewModel = ViewModelProvider(this, factory)[VideosViewModel::class.java]
         setupUI()
         loadData()
     }
@@ -72,6 +82,13 @@ class MovieListFragment: Fragment() {
             }
         }
     }
+
+    private val onListItemClickListener: OnListItemClickListener =
+        object: OnListItemClickListener{
+            override fun onItemClick(data: VideoItem) {
+                router.navigateTo(Screens.DetailsScreen(data.id))
+            }
+        }
 
     override fun onDestroy() {
         super.onDestroy()
